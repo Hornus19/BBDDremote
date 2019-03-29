d28d78e0ceaee94e62ea1f142f7cd27b3eaf8fff
David <hornus19@hotmail.com>
2019-03-29 18:00:04 +0100

    Segundo Commit
    Ya se conecta a la bbdd y realiza una consulta simple

--- app/src/main/java/com/example/bbddonline/MainActivity.java
+++ app/src/main/java/com/example/bbddonline/MainActivity.java
@@ -4,20 +4,23 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private TextView texto;
     private Statement st;
     private ResultSet rs;
     private Connection con;
-    private String baseDatos = "catastro_ex";
+    private String baseDatos = "empresab";
+    private ArrayList<String> listado = new ArrayList<>(0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +28,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+        texto = (TextView) findViewById(R.id.texto);
         try {
-        String driver="com.mysql.jdbc.Driver";
-        String urlMysql = "jdbc:mysql://213.60.153.125:3306/";
-        Class.forName(driver).newInstance();
-        con= DriverManager.getConnection(urlMysql+baseDatos,"david","alba");
-       // st=con.createStatement();
-            Toast.makeText(MainActivity.this,"Se conecta correctamente",Toast.LENGTH_LONG).show();
+            String driver = "com.mysql.jdbc.Driver";
+            String urlMysql = "jdbc:mysql://213.60.153.125:3306/";
+            Class.forName(driver).newInstance();
+            con = DriverManager.getConnection(urlMysql + baseDatos, "david", "Alba123.");
+            st = con.createStatement();
+            rs = st.executeQuery("Select * from departamentos");
+
+            while (rs.next()) {
+                listado.add(rs.getString("NOMBRE"));
+            }
+            texto.setText(listado.get(0));
         } catch (Exception e) {
             e.printStackTrace();
-            Log.i("hola",e.getMessage());
-            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+            Log.i("hola", e.getMessage());
         }
     }
 
